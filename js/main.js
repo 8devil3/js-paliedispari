c9e303a8fa2f82a4f2cb668202d386ff89254582
8devil3 <8dev.il3@gmail.com>
2022-03-01 16:52:19 +0100

    aggiunto stile al pari e dispari

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* Palidroma
-Chiedere all’utente di inserire una parola
+Chiedere all’utente di inserire una parola.
 Creare una funzione per capire se la parola inserita è palindroma */
 
 const btnCheck = document.querySelector('#sn1-btn');
@@ -7,10 +7,7 @@ const input1 = document.querySelector('#sn1-input');
 const msgHTML1 = document.querySelector("#sn1-msg");
 
 
-
 btnCheck.addEventListener('click', checkFinal);
-
-
 
 function checkFinal() {
     msgHTML1.classList.remove('true', 'false');
@@ -32,9 +29,6 @@ function checkFinal() {
     }
 
 }
-
-
-
 
 
 function checkPalindrome(word) {
@@ -63,28 +57,74 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
 
+const btnPlay = document.querySelector('#sn2-btn');
+const input2chooseNum = document.querySelector('#sn2-input');
+const input2oddEven = document.querySelector('#sn2-select');
+const msgHTML2 = document.querySelector("#sn2-msg");
 
-//const oddEvenChoosen = 'pari';
-// const numberChoosen = '2';
-
-
-// if (!checkOddEven(numberChoosen)) {
-//     console.log('CPU WIN!');
-// } else {
-//     console.log('You WIN!');
-// }
+const msgHTMLscore = document.querySelector("#score");
 
 
-// function rndmDigit() {
-//     return Math.floor(Math.random() * 5) + 1;;
-// }
 
-// function checkOddEven(num) {
+btnPlay.addEventListener('click', checkPlay);
+
+function checkPlay() {
+
+    msgHTML2.classList.remove('true', 'false');
+    msgHTMLscore.innerHTML = '';
+
+    if (input2chooseNum.value == '') { // controllo se i campi sono vuoti
+        msgHTML2.innerHTML = "Digita un numero!";
+        msgHTML2.classList.add("false");
+    } else if (parseInt(input2chooseNum.value) > 5 || parseInt(input2chooseNum.value) <= 0) {
+        msgHTML2.innerHTML = "Il numero deve essere compreso tra 1 e 5!";
+        msgHTML2.classList.add("false");
+    } else {
+
+        if (checkOddEven(parseInt(input2chooseNum.value)) == input2oddEven.value) {
+            msgHTML2.classList.add('true');
+            msgHTML2.innerHTML = 'Hai vinto!';
+
+            msgHTMLscore.innerHTML = 'La somma dei due numeri è: ' + sum;
+            
+        } else {
+            msgHTML2.classList.add('false');
+            msgHTML2.innerHTML = 'Hai perso!';
+
+            msgHTMLscore.innerHTML = 'La somma dei due numeri è: ' + sum;
+        }
+    }
+}
+
+function rndmDigit() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+
+function checkOddEven(num) {
+
+    sum = num + rndmDigit(); // dichiaro la variabile "nuda" per recuperare il valore anche al di fuori della funzione
     
-//     if ((num + rndmDigit()) % 2 != 0) {
-//         return false;
-//     } else {
-//         return true;
-//     }
-// }
+    if (sum % 2 != 0) {
+        return 'odd';
+    } else {
+        return 'even';
+    }
+}
 
+
+
+
+
+
+
+// reset inputs e ricarica della pagina
+const btnReload = document.querySelector("#reload");
+const arrInputs = document.querySelectorAll('input');
+
+btnReload.addEventListener('click', function() {
+    for (x = 0; x < arrInputs.length ; x++) { // reset dei campi
+        arrInputs[x].value ='';
+    }
+    location.reload(); // ricarico la pagina
+})
