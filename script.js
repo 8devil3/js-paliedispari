20522782dccff94cea450ca2849202ee42cf71a3
8devil3 <8dev.il3@gmail.com>
2022-03-01 14:54:41 +0100

    funzione palindroma

--- script.js
+++ script.js
@@ -3,19 +3,34 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
 
 
-let word = prompt('Type a word');
+let userWord = prompt('Type a word').toLowerCase().replaceAll(' ', '');
 const arrWordCheck = [];
 let palindrome;
 
 
-for (let i = word.length - 1; i >= 0; i--) {
-    arrWordCheck.push(word[i])
-}
-
-palindrome = arrWordCheck.toString().replaceAll(',', '');
-
-if (palindrome != word) {
-    console.log('Not a palindrome.')
+if (checkPalindrome(userWord)) {
+    console.log('This is a palindrome.');
 } else {
-    console.log('This is a palindrome.')
+    console.log('Not a palindrome.');
 }
+
+
+
+function checkPalindrome(word) {
+    
+    for (let i = word.length - 1; i >= 0; i--) {
+        arrWordCheck.push(word[i])
+    }
+    
+    palindrome = arrWordCheck.toString().replaceAll(',', '');
+    
+    if (palindrome != word) {
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+
+
